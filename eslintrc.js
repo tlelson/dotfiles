94b6227aa5cb7cdb95af3a766ee1043f4998f6bd
eL0ck <tpj800@gmail.com>
2017-10-06 18:32:07 +1000

    eslint options

--- eslintrc.js
+++ eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
   "extends": "airbnb",
   "plugins": [],
   "rules": {
+    "semi": "off",
     "func-names": "off",
 		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
 		// Following fix for un-understood error
 		"jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }],
     // doesn't work in node v4 :(
     "strict": "off",
     "prefer-rest-params": "off",
