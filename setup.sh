11321d80260330b5d505ec54c0bb0e94df75ce7a
eL0ck <tpj800@gmail.com>
2017-11-24 20:39:10 +1000

    shouldnt be copying the .vim dir.  It is generated by dein

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 
-for file in bashrc  bash_profile  htoprc gitconfig  tmux.conf gitignore  vim  vimrc  jshintrc gitattributes eslintrc.js
+for file in bashrc  bash_profile  htoprc gitconfig  tmux.conf gitignore  vimrc  jshintrc gitattributes eslintrc.js
 do
     echo "looking for ~/.${file} .."
     if [ -h ~/.${file} ]; then # Is it a sybolic link ?
