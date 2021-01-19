918c4953a9467e6198a40376bbcfa13bc6cf1f8e
tim <tpj800@gmail.com>
2021-01-20 08:30:46 +1100

    Stop Global Node Installs needing Root
    This sets a user owned directory as the global node_modules location

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Copying dotfiles into location ..."
-for file in mybashrc  bash_profile  htoprc gitconfig  gitignore  jshintrc gitattributes eslintrc.js jnettop tigrc tmux.conf.local fzf.bash ctags ripgreprc
+for file in mybashrc  bash_profile  htoprc gitconfig  gitignore  jshintrc gitattributes eslintrc.js jnettop tigrc tmux.conf.local fzf.bash ctags ripgreprc npmrc
 do
     echo "looking for ~/.${file} .."
     if [ -h ~/.${file} ]; then # Is it a sybolic link ?
