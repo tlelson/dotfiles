a2db2da7922c504a28331a1080b65138b8bab092
tim <tpj800@gmail.com>
2022-04-30 09:24:36 +1000

    Neovim: Se background to be same as terminal

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -5,4 +5,6 @@ catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
+" Reset the background to the normal one in the terminal
+highlight Normal guibg=none guifg=none
 ]]
