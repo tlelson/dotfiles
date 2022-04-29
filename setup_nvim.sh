ce21315915de3aa6360b390ddf1f7697e517f6eb
tim <tpj800@gmail.com>
2022-04-30 09:38:35 +1000

    Neovim: Fix setup Script for OSX
    Automatically linking to `vim`

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -21,16 +21,20 @@ ln -s $(pwd)/nvim ~/.config/nvim
 echo "dotfiles/nvim linked to ~/.config/nvim ..."
 
 if [[ $OSTYPE == "linux"* ]]; then
-	echo 'Downloading latest stable release of neo-vim .... '
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-	chmod u+x /etc/alternatives/nvim
-fi
+    echo 'Downloading latest stable release of neo-vim .... '
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x /etc/alternatives/nvim
 
-echo "Call with 'nvim' or relink manually ..."
-#rm $(which vim)
-#ln -s /usr/local/bin/vim /etc/alternatives/nvim
+    echo "Call with 'nvim' or relink manually ..."
+    #rm $(which vim)
+    #ln -s /usr/local/bin/vim /etc/alternatives/nvim
+
+elif [[ $OSTYPE == "darwin"* ]]; then
+    echo 'Neo-vim alread downloaded by `setup.sh`. Linking `vim` to `nvim` ...'
+    ln -s $(which nvim) $(brew --prefix)/bin/vim
+fi
 
 echo "3.    Open vim and install plugins with:"
 echo "          :PackerInstall"
