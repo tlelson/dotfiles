ffb4330aebd08438f8b4e503b089189a86f66f29
tim <tpj800@gmail.com>
2022-05-30 09:09:39 +1000

    NeoVim: Add mapping to scoll autocomplete options

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -29,6 +29,8 @@ cmp.setup {
     end,
   },
   mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
