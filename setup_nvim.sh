32bb89d171ebba49615692686913669a560587b4
tim <tpj800@gmail.com>
2023-11-30 14:15:46 +1100

    change to LazyVim for neovim
     - This changes where fzf comes from

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -2,34 +2,38 @@
 
 # Setup Config
 echo "Making a .config/nvim/ for configuration"
-if [ -d ~/.config/nvim ];
-    then
-        echo '~/.config/nvim already exists. Moving to /tmp . Review it before next shutdown!';
-        mv ~/.config/nvim /tmp
-    else
-        echo '~/.config/nvim does not exist or is not a directory.';
+if [ -d ~/.config/nvim ]; then
+	echo '~/.config/nvim already exists. Moving to /tmp . Review it before next shutdown!'
+	# required
+	mv ~/.config/nvim{,.bak}
+
+	# optional but recommended
+	mv ~/.local/share/nvim{,.bak}
+	mv ~/.local/state/nvim{,.bak}
+	mv ~/.cache/nvim{,.bak}
+else
+	echo '~/.config/nvim does not exist or is not a directory.'
 fi
 
-if [ ! -d ~/.config ];
-then
-    mkdir ~/.config
+if [ ! -d ~/.config ]; then
+	mkdir ~/.config
 fi
 
 git clone https://github.com/tlelson/nvim.git ~/.config/nvim
 
 if [[ $OSTYPE == "linux"* ]]; then
-    echo 'Downloading latest stable release of neo-vim .... '
-    curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-    chmod +x /etc/alternatives/nvim
+	echo 'Downloading latest stable release of neo-vim .... '
+	curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+	chmod +x /etc/alternatives/nvim
 
-    echo "Call with 'nvim' or relink manually ..."
-    #rm $(which vim)
-    #ln -s /etc/alternatives/nvim /usr/local/bin/nvim
+	echo "Call with 'nvim' or relink manually ..."
+	#rm $(which vim)
+	#ln -s /etc/alternatives/nvim /usr/local/bin/nvim
 
 elif [[ $OSTYPE == "darwin"* ]]; then
-    echo 'Neo-vim alread downloaded by `setup.sh`. Linking `vim` to `nvim` ...'
-    ln -s $(which nvim) $(brew --prefix)/bin/vim
+	echo 'Neo-vim alread downloaded by `setup.sh`. Linking `vim` to `nvim` ...'
+	ln -s $(which nvim) $(brew --prefix)/bin/vim
 fi
 
 echo "3.    Open vim and install plugins with:"
-echo "          :PackerInstall"
+echo "          :Lazy
