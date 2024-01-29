b643a94f9c71870d096efaa4a104710dee818895
tim <tpj800@gmail.com>
2024-01-29 11:38:24 +1100

    Update tmux starter

--- tmux-env-starter.sh
+++ tmux-env-starter.sh
@@ -4,16 +4,24 @@
 
 tmux new-session -s 'DEV' -n 'Ref' -c "$HOME/notes" -d
 tmux send-keys -t 'Ref' 'git st' Enter
-tmux split-window -h -c "$HOME/.aws"  # No idea how this knows which window to split.
+tmux split-window -h -c "$HOME/.config/nvim" # No idea how this knows which window to split.
 tmux send-keys 'tree' Enter
 tmux new-window -n 'OldSpice Notes' -c "$HOME/client/Kablamo/OldSpice/"
 ## if sending this via cmd above, window will close after exit
 tmux send-keys -t 'OldSpice Notes' 'vim notes.txt' Enter
+
 # Open some windows for the code
-tmux new-window -n 'OldSpice Code' -c "$HOME/client/Kablamo/OldSpice/code"
-tmux send-keys -t 'OldSpice Code' 'ls' Enter
+tmux new-window -n 'osfin' -c "$HOME/client/Kablamo/OldSpice/code/osfin"
+tmux send-keys -t 'osfin' 'ls' Enter
 
-tmux new-window -n 'BoatShed' -c "$HOME/client/Boatshed" #'vim'
+# Open some windows for the code
+tmux new-window -n 'osfin-security' -c "$HOME/client/Kablamo/OldSpice/osfin-security"
+tmux send-keys -t 'osfin-security' 'ls' Enter
 
+# Open some windows for the code
+tmux new-window -n 'osfin-customer' -c "$HOME/client/Kablamo/OldSpice/osfin-customer"
+tmux send-keys -t 'osfin-customer' 'ls' Enter
+
+# Set focus on Notes window
 tmux select-window -t 'DEV':'OldSpice Notes'
 tmux attach-session -t 'DEV' -d
