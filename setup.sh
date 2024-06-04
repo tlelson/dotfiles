951bfa41d8297af1835ffd426eb09b16e3a03995
tim <tpj800@gmail.com>
2024-06-05 07:48:57 +1000

    Fixup up setup scripts after mac os install

--- setup.sh
+++ setup.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
 echo "Copying dotfiles into location ..."
-for file in mybashrc  bash_profile  htoprc gitconfig  gitignore  jshintrc gitattributes eslintrc.js jnettop tigrc tmux.conf.local ctags ripgreprc npmrc yamllint.yaml
-do
-    echo "looking for ~/.${file} .."
-    if [ -h ~/.${file} ]; then # Is it a sybolic link ?
-        echo "  Already exists as symbolic link. Not linking to new dotfiles version.  If you want it remove your current one ..."
-    elif [ -e ~/.${file}  ]; then # Does it exist (implies not symbolic link)?
-        echo "  File exists.  Moving to ${file}_old"
-        mv ~/.${file} ~/.${file}_old
-        ln -s $(pwd)/${file} ~/.${file}
-    else
-        echo "  File not found. Linking to new dotfiles version ... "
-        ln -s $(pwd)/${file} ~/.${file}
-    fi
+for file in mybashrc bash_profile htoprc gitconfig gitignore jshintrc gitattributes eslintrc.js jnettop tigrc tmux.conf.local ctags ripgreprc npmrc yamllint.yaml; do
+	echo "looking for ~/.${file} .."
+	if [ -h ~/.${file} ]; then # Is it a sybolic link ?
+		echo "  Already exists as symbolic link. Not linking to new dotfiles version.  If you want it remove your current one ..."
+	elif [ -e ~/.${file} ]; then # Does it exist (implies not symbolic link)?
+		echo "  File exists.  Moving to ${file}_old"
+		mv ~/.${file} ~/.${file}_old
+		ln -s $(pwd)/${file} ~/.${file}
+	else
+		echo "  File not found. Linking to new dotfiles version ... "
+		ln -s $(pwd)/${file} ~/.${file}
+	fi
 done
 
-echo 'Copy config files into `.config` ...' 
-for file in $(ls ./config)
-do 
+echo 'Copy config files into `.config` ...'
+for file in $(ls ./config); do
 	echo "ln -s $(pwd)/config/$file ~/.config/$file"
 done
 
 # Set use my bash config
 echo "Sourcing '~/.mybashrc' from system '~/.bashrc' ... "
-echo "source ~/.mybashrc" >> ~/.bashrc
+echo "source ~/.mybashrc" >>~/.bashrc
 
 # Setup Ipython preferences
 # Notice that two files go up.  The higher one has access to the `c` config object
@@ -33,6 +31,9 @@ echo "Setting up ipython auto start config ..."
 mkdir -p ~/.ipython/profile_default/startup/
 ln -s $(pwd)/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 ln -s $(pwd)/ipython_setup.ipy ~/.ipython/profile_default/startup/ipython_setup.ipy
+
+mkdir -p ~/.w3m
+ln -s $(pwd)/w3m-keymap ~/.w3m/keymap
 
 # Set up TMUX
 echo 'Using tmux config from "https://github.com/gpakosz/.tmux".  See repo for details ...'
@@ -51,22 +52,22 @@ if [[ $OSTYPE == "darwin"* ]]; then
 	ln -s $(pwd)/bashrc_osx ~/.bashrc_os
 	echo "Setting up python/node sandbox ..."
 	conda create -y -n general python ipython nodejs
-elif ( command -v dnf  ); then
+elif (command -v dnf); then
 	echo "'dnf' found. Assuming CentOS-ish ..."
 	sudo dnf update && sudo dnf upgrade -y
-	sudo dnf install -y $(grep -v "#" dnf-packs | xargs )
+	sudo dnf install -y $(grep -v "#" dnf-packs | xargs)
 	ln -s $(pwd)/bashrc_linux ~/.bashrc_os
 	conda create -y -n general python ipython nodejs
-elif ( command -v yum  ); then
+elif (command -v yum); then
 	echo "'yum' found. Assuming Old CentOS-ish ..."
 	sudo yum update && sudo yum upgrade -y
-	sudo yum install -y $(grep -v "#" dnf-packs | xargs )
+	sudo yum install -y $(grep -v "#" dnf-packs | xargs)
 	ln -s $(pwd)/bashrc_linux ~/.bashrc_os
 	conda create -y -n general python ipython nodejs
-elif ( command -v apt ); then
+elif (command -v apt); then
 	echo "'apt' found. Assuming debian ..."
 	sudo apt update && sudo apt upgrade -y
-	sudo apt install -y $(grep -v "#" apt-packs | xargs )
+	sudo apt install -y $(grep -v "#" apt-packs | xargs)
 	ln -s $(pwd)/bashrc_linux ~/.bashrc_os
 	echo 'no easy way to install conda. Do it manually'
 else
