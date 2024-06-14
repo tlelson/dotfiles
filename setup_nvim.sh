f0a5d2f80f8f86559c6600287452700b278542fc
tlelson <tpj800@gmail.com>
2024-06-14 10:20:08 +1000

    Update NeoVim install

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -23,16 +23,17 @@ git clone https://github.com/tlelson/nvim.git ~/.config/nvim
 
 if [[ $OSTYPE == "linux"* ]]; then
 	echo 'Downloading latest stable release of neo-vim .... '
-	curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-	chmod +x /etc/alternatives/nvim
+	sudo curl -fLo /etc/alternatives/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+	sudo chmod +x /etc/alternatives/nvim
 
 	echo "Call with 'nvim' or relink manually ..."
 	#rm $(which vim)
-	#ln -s /etc/alternatives/nvim /usr/local/bin/nvim
+	sudo ln -s /etc/alternatives/nvim /usr/local/bin/nvim
+	sudo ln -s "$(which nvim)" "$(which vim)"
 
 elif [[ $OSTYPE == "darwin"* ]]; then
 	echo 'Neo-vim alread downloaded by `setup.sh`. Linking `vim` to `nvim` ...'
-	ln -s $(which nvim) $(brew --prefix)/bin/vim
+	sudo ln -s "$(which nvim)" "$(brew --prefix)/bin/vim"
 fi
 
 echo "3.    Open vim and install plugins with:"
