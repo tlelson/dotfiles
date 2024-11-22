776f73b52c82efe02ee3a374624aae8e9b694170
tim <tpj800@gmail.com>
2024-11-22 11:00:54 +1100

    Add General Nix Shell for Ipython and Alias

---
+++ nix-shells/ipython.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    python312Packages.ipython
+    python312Packages.numpy
+  ];
+  shellHook = ''
+    ipython
+  '';
+}
